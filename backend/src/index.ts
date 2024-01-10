4a7d89ab790979c211514fd7b414792291a66a06
dany <danylobyshkin@gmail.com>
2024-01-10 13:19:41 -0500

    register user done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import * as db from './db/database';
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.json())
@@ -21,9 +22,20 @@ app.get('/get-users', async(req :Request, res: Response, next :NextFunction) => 
     
 })
 
-app.post('/create-user', (req :Request, res :Response) => {
+app.post('/register', async (req :Request, res :Response) => {
+
+    const {username, password} = req.body;
+    console.log(username, password)
     try{
+        const existingUser = await db.query("SELECT * FROM users WHERE username = $1", [username])
+        if(existingUser.rows.length > 0){
+            return res.status(400).send("Username is already taken");
+        }
         
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const result = await db.query("INSERT INTO users (username, password) VALUES ($1 ,$2)", [username, hashedPassword]);
+        res.send("Success")
+
     } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ error: 'Internal Server Error' });
