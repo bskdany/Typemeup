d712c90b4f2114ff54d46d90fe404c05f1cf126e
bskdany <54674556+bskdany@users.noreply.github.com>
2024-08-05 10:37:46 -0400

    made the toast type non mandatory and default to error

--- frontend/src/shared/toastController.svelte.ts
+++ frontend/src/shared/toastController.svelte.ts
@@ -1,12 +1,12 @@
 export type Toast = {
   message: string;
   duration?: number;
-  type: 'success' | 'error' | 'info'
+  type?: 'success' | 'error' | 'info'
 }
 
 export const toastList: Toast[] = $state([]);
 
-export function showToast({ message, duration = 3000, type }: Toast) {
+export function showToast({ message, duration = 3000, type = 'error' }: Toast) {
   toastList.push({ message, duration, type });
 
   setTimeout(() => {
