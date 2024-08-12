9bf3b4f39d450b89e52b8b5ed639d8290fbe6cf2
bskdany <54674556+bskdany@users.noreply.github.com>
2024-08-11 21:39:18 -0400

    removing the letter frequency consideration because it would be a double penalty

--- frontend/src/algo/updateKeyStatistics.ts
+++ frontend/src/algo/updateKeyStatistics.ts
@@ -36,21 +36,12 @@ function normalizeValue(value: number, min: number, max: number) {
 }
 
 function generateScores(keyStatistics: KeyStatistic[]) {
-  const letterFrequency = new Map<string, number>([
-    ['a', 8.17], ['b', 1.49], ['c', 2.78], ['d', 4.25], ['e', 12.70],
-    ['f', 2.23], ['g', 2.02], ['h', 6.09], ['i', 6.97], ['j', 0.15],
-    ['k', 0.77], ['l', 4.03], ['m', 2.41], ['n', 6.75], ['o', 7.51],
-    ['p', 1.93], ['q', 0.10], ['r', 5.99], ['s', 6.33], ['t', 9.06],
-    ['u', 2.76], ['v', 0.98], ['w', 2.36], ['x', 0.15], ['y', 1.97],
-    ['z', 0.07]
-  ]);
-
   const minWpm = Math.min(...keyStatistics.map(entry => entry.wpm));
   const maxWpm = Math.max(...keyStatistics.map(entry => entry.wpm));
   const minAccuracy = Math.min(...keyStatistics.map(entry => entry.accuracy));
   const maxAccuracy = Math.max(...keyStatistics.map(entry => entry.accuracy));
-  const minTotalCount = Math.min(...keyStatistics.map(entry => (entry.totalHitCount) * (letterFrequency.get(entry.key) ?? 0)));
-  const maxTotalCount = Math.max(...keyStatistics.map(entry => (entry.totalHitCount) * (letterFrequency.get(entry.key) ?? 0)));
+  const minTotalCount = Math.min(...keyStatistics.map(entry => (entry.totalHitCount)));
+  const maxTotalCount = Math.max(...keyStatistics.map(entry => (entry.totalHitCount)));
 
   for (const keyStatistic of keyStatistics) {
 
