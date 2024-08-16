81fd14fdbc3e5aac35d07719a50be231bb1dea7c
bskdany <54674556+bskdany@users.noreply.github.com>
2024-08-16 14:44:50 -0400

    backend config fixed wrong env name

--- backend/src/lib/config.ts
+++ backend/src/lib/config.ts
@@ -2,7 +2,7 @@ export const isProd = process.env.NODE_ENV === "production";
 export const isDocker = process.env.IS_DOCKER === "true";
 const db_path: string = isDocker ? "/data/typemeup_prod.sqlite" : "./typemeup_dev.sqlite";
 const port_backend = parseInt(process.env.PORT_BACKEND ?? "3000") ?? 3000;
-const frontend_url = process.env.VITE_URL_FRONTEND ?? 'http://localhost:5173';
+const frontend_url = process.env.URL_FRONTEND ?? 'http://localhost:5173';
 
 export const config: Config = {
   is_prod: isProd,
